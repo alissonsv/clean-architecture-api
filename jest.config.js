6188c6efd9f6b56505dafc292582990311f54b0a
Alisson Vargas <contato@alissonsv.com>
2023-03-10 17:34:42 -0300

    chore: add jest configs for mongodb

--- jest.config.js
+++ jest.config.js
@@ -1,18 +1,21 @@
 module.exports = {
   // The directory where Jest should output its coverage files
-  coverageDirectory: "coverage",
+  coverageDirectory: 'coverage',
 
   // Indicates which provider should be used to instrument code for coverage
-  coverageProvider: "v8",
+  coverageProvider: 'v8',
 
   // A list of paths to directories that Jest should use to search for files in
   roots: ['<rootDir>/src'],
 
   // The test environment that will be used for testing
-  testEnvironment: "node",
+  testEnvironment: 'node',
 
   // A map from regular expressions to paths to transformers
   transform: {
     '.+\\.ts$': 'ts-jest'
   },
-};
+
+  // https://github.com/shelfio/jest-mongodb
+  preset: '@shelf/jest-mongodb'
+}
