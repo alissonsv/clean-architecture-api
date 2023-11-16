d8c5bb303e49fbac60ba1890b8c06c2712413800
Alisson Vargas <contato@alissonsv.com>
2023-11-16 00:17:39 -0300

    test: ensure ValidationComposite not return if validation succeeds

--- src/presentation/helpers/validators/validation-composite.spec.ts
+++ src/presentation/helpers/validators/validation-composite.spec.ts
@@ -43,4 +43,11 @@ describe('Validation Composite', () => {
 
     expect(error).toEqual(new Error())
   })
+
+  test('Should not return if validation succeeds', async () => {
+    const { sut } = makeSut()
+    const error = sut.validate({ field: 'any_value ' })
+
+    expect(error).toBeUndefined()
+  })
 })
